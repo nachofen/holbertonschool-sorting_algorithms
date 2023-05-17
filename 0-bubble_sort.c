c083eb1465cb37bd77dbf472b4bbd1a9654b3cb4
nachofen <zorba037@gmail.com>
2023-05-17 09:48:19 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+void
+bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, aux = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
+			}
+		}
+	}
+}
