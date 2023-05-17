3bc92200fb2858ae1c59c683827b5bf99c1909ea
nachofen <zorba037@gmail.com>
2023-05-17 09:58:53 -0700

    betty i hate you

--- sort.h
+++ sort.h
@@ -13,8 +13,8 @@
 typedef struct listint_s
 {
 	const int n;
-    	struct listint_s *prev;
-    	struct listint_s *next;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
