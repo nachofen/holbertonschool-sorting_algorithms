399458baba58371db6a3db98fd7a6e6d234fdbf5
nachofen <zorba037@gmail.com>
2023-05-17 09:57:07 -0700

    task 0, biGO

--- sort.h
+++ sort.h
@@ -1,3 +1,5 @@
+#ifndef sort_h
+#define sort_h
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,12 +12,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+    	struct listint_s *prev;
+    	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
+#endif
